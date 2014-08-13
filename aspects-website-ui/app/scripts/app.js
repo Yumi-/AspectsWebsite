3fd099d0b88892bf06ec25624c19f0507da05435
Quentin <qrichert@gmail.com>
2014-08-14 00:12:18 +0200

    Initial commit

---
+++ aspects-website-ui/app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name aspectsWebsiteUiApp
+ * @description
+ * # aspectsWebsiteUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('aspectsWebsiteUiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource'
+  ]);
